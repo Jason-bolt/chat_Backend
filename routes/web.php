64b6daf52736b6021682209620663e86b54a87f5
Jason-bolt <appiatujason@gmail.com>
2022-09-16 11:11:09 +0000

    npm installed

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\Controller::class, 'index']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
